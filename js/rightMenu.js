b63542e7d42692aa0385e0e548d3e9e2b25d3259
KayChen <kaychenvip@gmail.com>
2023-07-19 10:49:07 +0800

    Site updated: 2023-07-19 10:49:07

--- js/rightMenu.js
+++ js/rightMenu.js
@@ -188,8 +188,3 @@ $("#rightmenu-mask").contextmenu(function () {
     return false;
 });
 
-
-作者: Ethan.Tzy
-链接: https://fe32.top/articles/hexo1608/#%E6%AD%A5%E9%AA%A4
-来源: 唐志远の博客
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
